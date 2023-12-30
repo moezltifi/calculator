2c45e984b1f356d60609d6ae0140688b0f4c4172
moezl <moez.mltifi@gmail.com>
2023-12-30 19:03:33 +0100

    correct mark proplemes

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ const buttons=document.getElementsByTagName("button")
 
 var valueToBeCalculated = '';
 var screenValue = "";
+const marksList = ["+","-","/","x","."]
 
 for(var i=0; i<buttons.length; i++){
     buttons[i].addEventListener("click" , handleButtonClick);
@@ -26,7 +27,37 @@ for(var i=0; i<buttons.length; i++){
                 valueToBeCalculated=screenValue
 
             }
+            else if (marksList.includes(this.innerText) && screenValue.length==0){
+                if (this.innerText=="x"){
+                    valueToBeCalculated="0"+"*";
+                    screenValue= "0"+ this.innerText;
+                }
+                else{
+                    valueToBeCalculated="0"+ this.innerText;
+                    screenValue="0"+ this.innerText;
+                }
+            }
 
+            else if (this.id=="point" && screenValue.length==0){
+                screenValue="0.";
+                valueToBeCalculated="0."
+            }
+            else if (this.id=="point" && screenValue.charAt(screenValue.length-1)=="."){
+                console.log("point")
+            }
+            else if ( marksList.includes(this.innerText) &&  marksList.includes(screenValue.charAt(screenValue.length-1))){
+                console.log("mark")
+                valueToBeCalculated=valueToBeCalculated.slice(0,valueToBeCalculated.length-1)
+                screenValue=screenValue.slice(0,screenValue.length-1)
+                if (this.innerText=="x"){
+                    valueToBeCalculated+="*";
+                    screenValue+=this.innerText;
+                }
+                else{
+                    valueToBeCalculated+=this.innerText;
+                    screenValue+=this.innerText;
+                }
+            }
             else if (this.id=="markx"){
                 valueToBeCalculated+="*";
                 screenValue+=this.innerText;
