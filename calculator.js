e708500432fcde549fbfdb9b7f972d057ce3edd6
moezl <moez.mltifi@gmail.com>
2023-12-30 15:27:52 +0100

    show button value on screen

--- calculator.js
+++ calculator.js
@@ -1,1 +1,12 @@
-document.getAnimations("number1")
+const buttons=document.getElementsByTagName("button")
+
+for(var i=0; i<buttons.length; i++){
+    buttons[i].addEventListener("click" , buttonsinner)
+
+    function buttonsinner(){
+       buttons[i]= document.getElementById("screen").innerHTML=this.innerText
+    }
+
+    
+}
+
