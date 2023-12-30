50aed75bc468df2041ccbbaad403318321e05fc3
moezl <moez.mltifi@gmail.com>
2023-12-30 15:39:55 +0100

    show multiple numbers on screen

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
 const buttons=document.getElementsByTagName("button")
 
+var screenValue = "";
+
 for(var i=0; i<buttons.length; i++){
     buttons[i].addEventListener("click" , buttonsinner)
 
     function buttonsinner(){
-       buttons[i]= document.getElementById("screen").innerHTML=this.innerText
+        if (this.id=="mark="){
+            screenValue = "";
+        }
+        else{
+            screenValue+=this.innerText
+        }
+        buttons[i]= document.getElementById("screen").innerHTML=screenValue
     }
 
     
