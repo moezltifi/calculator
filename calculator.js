681d2efc3993ccd0a210469d4999f52c26b348fb
moezl <moez.mltifi@gmail.com>
2023-12-30 17:16:32 +0100

    add NaN resalt for wrong expressions

--- calculator.js
+++ calculator.js
@@ -4,24 +4,32 @@ var valueToBeCalculated = '';
 var screenValue = "";
 
 for(var i=0; i<buttons.length; i++){
-    buttons[i].addEventListener("click" , buttonsinner);
+    buttons[i].addEventListener("click" , handleButtonClick);
 
-    function buttonsinner(){
-        if (this.id=="mark="){
-            console.log((valueToBeCalculated).toString());
-            screenValue=eval(valueToBeCalculated).toString();
-            valueToBeCalculated=screenValue
+    function handleButtonClick(){
+        try{
+            if (this.id=="mark="){
+                console.log((valueToBeCalculated).toString());
+                screenValue=eval(valueToBeCalculated).toString();
+                valueToBeCalculated=screenValue
 
+            }
+            else if (this.id=="markx"){
+                valueToBeCalculated+="*";
+                screenValue+=this.innerText;
+            }
+            else{
+                valueToBeCalculated+=this.innerText;
+                screenValue+=this.innerText;
+            }
+            document.getElementById("screen").innerHTML=screenValue;
         }
-        else if (this.id=="markx"){
-            valueToBeCalculated+="*";
-            screenValue+=this.innerText;
+        catch{
+            valueToBeCalculated="";
+            screenValue="";
+            document.getElementById("screen").innerHTML="NaN";
         }
-        else{
-            valueToBeCalculated+=this.innerText;
-            screenValue+=this.innerText;
-        }
-        document.getElementById("screen").innerHTML=screenValue;
+        
     }
 
     
